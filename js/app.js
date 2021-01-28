7464b386a36ae49d1709e3952c995abe21da63b8
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2021-01-27 21:32:59 -0500

    temp remove animations

--- js/app.js
+++ js/app.js
@@ -1,150 +1,150 @@
-gsap.to(".info_0", {
-    scrollTrigger: '.info_0',
-    y: -75,
-    duration: .75
-})
+// gsap.to(".info_0", {
+//     scrollTrigger: '.info_0',
+//     y: -75,
+//     duration: .75
+// })
 
-let ill0 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".info_0"
-    }
-});
+// let ill0 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".info_0"
+//     }
+// });
 
-ill0.from(".info_0", { opacity: 0, duration: 1})
+// ill0.from(".info_0", { opacity: 0, duration: 1})
 
-gsap.to(".info_1", {
-    scrollTrigger: '.info_1',
-    y: -75,
-    duration: .75
-})
+// gsap.to(".info_1", {
+//     scrollTrigger: '.info_1',
+//     y: -75,
+//     duration: .75
+// })
 
-let ill = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".info_1"
-    }
-});
+// let ill = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".info_1"
+//     }
+// });
 
-ill.from(".info_1", { opacity: 0, duration: 1})
-
-
-gsap.to(".aboutme", {
-    scrollTrigger: {
-        trigger: ".aboutme",
-        duration: .75,
-    },
-    y: -75,
-})
-
-let al = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".aboutme"
-    }
-});
-
-al.from(".aboutme", { opacity: 0, duration: 1})
+// ill.from(".info_1", { opacity: 0, duration: 1})
 
 
-gsap.to("footer", {
-    scrollTrigger: {
-        trigger: ".footer",
-        duration: .75,
-    },
-    y: -75,
-})
+// gsap.to(".aboutme", {
+//     scrollTrigger: {
+//         trigger: ".aboutme",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
 
-let fl = gsap.timeline({
-    scrollTrigger: {
-        trigger: "footer"
-    }
-});
+// let al = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".aboutme"
+//     }
+// });
 
-fl.from("footer", { opacity: 0, duration: 1})
-
-
-gsap.to(".hero_inner", {
-    scrollTrigger: {
-        trigger: ".hero_inner",
-        duration: .75,
-    },
-    // y: 0,
-    scale: .75
-})
-
-let hl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".hero_inner"
-    }
-});
-
-hl.from(".hero_inner", { scale: 1.2, opacity: 0, duration: .75})
-
-///////
-
-gsap.to(".info_box-0", {
-    scrollTrigger: {
-        trigger: ".info_box-0",
-        duration: .75,
-    },
-    y: -75,
-})
-
-let info0 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".info_box-0"
-    }
-});
-
-info0.from(".info_box-0", { opacity: 0, duration: 1})
+// al.from(".aboutme", { opacity: 0, duration: 1})
 
 
-gsap.to(".info_box-1", {
-    scrollTrigger: {
-        trigger: ".info_box-1",
-        duration: .75,
-    },
-    y: -75,
-})
+// gsap.to("footer", {
+//     scrollTrigger: {
+//         trigger: ".footer",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
 
-let info1 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".info_box-1"
-    }
-});
+// let fl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: "footer"
+//     }
+// });
 
-info1.from(".info_box-1", { opacity: 0, duration: 1})
+// fl.from("footer", { opacity: 0, duration: 1})
 
 
-gsap.to(".info_box-2", {
-    scrollTrigger: {
-        trigger: ".info_box-2",
-        duration: .75,
-    },
-    y: -75,
-})
+// gsap.to(".hero_inner", {
+//     scrollTrigger: {
+//         trigger: ".hero_inner",
+//         duration: .75,
+//     },
+//     // y: 0,
+//     scale: .75
+// })
 
-let info2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".info_box-2"
-    }
-});
+// let hl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".hero_inner"
+//     }
+// });
 
-info2.from(".info_box-2", { opacity: 0, duration: 1})
+// hl.from(".hero_inner", { scale: 1.2, opacity: 0, duration: .75})
 
-////////
+// ///////
 
-gsap.to(".work", {
-    scrollTrigger: {
-        trigger: ".work",
-        duration: .75,
-    },
-    y: -75,
-})
+// gsap.to(".info_box-0", {
+//     scrollTrigger: {
+//         trigger: ".info_box-0",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
 
-let work0 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".work"
-    }
-});
+// let info0 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".info_box-0"
+//     }
+// });
 
-work0.from(".work", { opacity: 0, duration: 1})
+// info0.from(".info_box-0", { opacity: 0, duration: 1})
+
+
+// gsap.to(".info_box-1", {
+//     scrollTrigger: {
+//         trigger: ".info_box-1",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
+
+// let info1 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".info_box-1"
+//     }
+// });
+
+// info1.from(".info_box-1", { opacity: 0, duration: 1})
+
+
+// gsap.to(".info_box-2", {
+//     scrollTrigger: {
+//         trigger: ".info_box-2",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
+
+// let info2 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".info_box-2"
+//     }
+// });
+
+// info2.from(".info_box-2", { opacity: 0, duration: 1})
+
+// ////////
+
+// gsap.to(".work", {
+//     scrollTrigger: {
+//         trigger: ".work",
+//         duration: .75,
+//     },
+//     y: -75,
+// })
+
+// let work0 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".work"
+//     }
+// });
+
+// work0.from(".work", { opacity: 0, duration: 1})
 
